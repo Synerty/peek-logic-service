8d5c242830ab8577ea5a37094f232eb2025e177c
brentonford <brenton.ford@synerty.com>
2016-12-22 07:50:51 -0500

    Revert "import had unresolved variable"
    This reverts commit d5a12cd17bd60946d85ff0eed86b52eb920063db.

--- peek_server/server/sw_install/PluginSwInstallManager.py
+++ peek_server/server/sw_install/PluginSwInstallManager.py
@@ -1,8 +1,8 @@
 from peek_platform import PeekPlatformConfig
-from peek_platform.sw_install.PluginSwInstallManagerBase import PluginSwInstallManagerBase
+from peek_platform.sw_install.PluginSwInstallManagerABC import PluginSwInstallManagerABC
 
 
-class PluginSwInstallManager(PluginSwInstallManagerBase):
+class PluginSwInstallManager(PluginSwInstallManagerABC):
     def notifyOfPluginVersionUpdate(self, pluginName, targetVersion):
         PeekPlatformConfig.pluginLoader.loadPlugin(pluginName)
         PeekPlatformConfig.pluginLoader.buildFrontend()
