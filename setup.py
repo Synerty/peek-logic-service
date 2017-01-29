ec353ba455cfe481cc04d5acb8f7ad4cafd32e7f
brentonford <brenton.ford@synerty.com>
2017-01-28 22:47:27 -0800

    required package update

--- setup.py
+++ setup.py
@@ -2,39 +2,43 @@ import os
 import shutil
 from distutils.core import setup
 
-from setuptools import find_packages
+pip_package_name = "peek-server"
+py_package_name = "peek_server"
 
-package_name = "peek-server"
 package_version = '0.0.11'
 
-egg_info = "%s.egg-info" % package_name
+egg_info = "%s.egg-info" % pip_package_name
 if os.path.isdir(egg_info):
     shutil.rmtree(egg_info)
 
-def package_files(startDir,
-                  excludeDirs=('__pycache__',),
-                  excludeFiles=('.pyc', '.js', '.js.map')):
-    startDir = os.path.join(package_name, startDir)
+excludePathContains = ('__pycache__', 'node_modules', 'platforms', 'dist')
+excludeFilesEndWith = ('.pyc', '.js', '.js.map', '.lastHash')
+excludeFilesStartWith = ()
 
+
+def find_package_files():
     paths = []
-    for (path, directories, filenames) in os.walk(startDir):
-        if [e for e in excludeDirs if e in path]:
+    for (path, directories, filenames) in os.walk(py_package_name):
+        if [e for e in excludePathContains if e in path]:
             continue
 
         for filename in filenames:
-            if [e for e in excludeFiles if e in filename]:
+            if [e for e in excludeFilesEndWith if filename.endswith(e)]:
                 continue
 
-            paths.append(os.path.join(path[len(package_name)+1:], filename))
+            if [e for e in excludeFilesStartWith if filename.startswith(e)]:
+                continue
+
+            paths.append(os.path.join(path[len(py_package_name) + 1:], filename))
 
     return paths
 
-package_data = package_files("alembic")
+package_files = find_package_files()
 
 setup(
-    name=package_name,
-    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
-    package_data={'': package_data},
+    name=pip_package_name,
+    packages=[py_package_name],
+    package_data={'': package_files},
     entry_points={
         'console_scripts': [
             'run_peek_server = peek_server.run_peek_server.main',
@@ -45,9 +49,9 @@ setup(
     description='Peek Platform - Server Service',
     author='Synerty',
     author_email='contact@synerty.com',
-    url='https://github.com/Synerty/%s' % package_name,
+    url='https://github.com/Synerty/%s' % pip_package_name,
     download_url='https://github.com/Synerty/%s/tarball/%s' % (
-        package_name, package_version),
+        pip_package_name, package_version),
     keywords=['Peek', 'Python', 'Platform', 'synerty'],
     classifiers=[
         "Programming Language :: Python :: 3.5",
