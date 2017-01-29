6b06976491fbd7a39f4be9fe3227d48a54d55737
Jarrod Chesney <jarrod.chesney@synerty.com>
2017-01-29 20:52:58 +1000

    Fixed config issue with setup.py console_scripts

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     package_data={'': package_files},
     entry_points={
         'console_scripts': [
-            'run_peek_server = peek_server.run_peek_server.main',
+            'run_peek_server = peek_server.run_peek_server:main',
         ],
     },
     install_requires=["peek-platform", "peek-server-fe"],
