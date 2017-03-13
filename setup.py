91d95d0a725f1a17a2b90413ff395a6c8b754d07
Jarrod Chesney <jarrod.chesney@synerty.com>
2017-03-13 16:16:11 +1000

    Updated to version 0.0.28

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 pip_package_name = "peek-server"
 py_package_name = "peek_server"
 
-package_version = '0.0.27'
+package_version = '0.0.28'
 
 egg_info = "%s.egg-info" % pip_package_name
 if os.path.isdir(egg_info):
