b038a05c8ed91a770c12b8ced17ab201a67119f8
Jarrod Chesney <jarrod.chesney@synerty.com>
2017-03-23 00:08:11 +1000

    Updated to version 0.2.9

--- peek_server/__init__.py
+++ peek_server/__init__.py
@@ -1,5 +1,5 @@
 __author__ = 'synerty'
-__version__ = '0.2.8'
+__version__ = '0.2.9'
 
 
 def importPackages():
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 pip_package_name = "peek-server"
 py_package_name = "peek_server"
 
-package_version = '0.2.8'
+package_version = '0.2.9'
 
 egg_info = "%s.egg-info" % pip_package_name
 if os.path.isdir(egg_info):
