07f095f12dd29c5265d5a617975b068b10b4d99d
Jarrod Chesney <jarrod.chesney@synerty.com>
2017-07-15 15:32:06 +1000

    All plugins are now loaded, before any are started

--- peek_server/plugin/ServerPluginLoader.py
+++ peek_server/plugin/ServerPluginLoader.py
@@ -2,10 +2,6 @@ import logging
 from typing import Type, Tuple
 
 import os
-
-# from peek_platform.plugin.PluginFrontendInstallerABC import PluginFrontendInstallerABC
-from twisted.internet.defer import inlineCallbacks
-
 from peek_platform.frontend.WebBuilder import WebBuilder
 from peek_platform.plugin.PluginLoaderABC import PluginLoaderABC
 from peek_plugin_base.PluginCommonEntryHookABC import PluginCommonEntryHookABC
@@ -15,6 +11,8 @@ from peek_plugin_base.server.PluginServerStorageEntryHookABC import \
 from peek_plugin_base.server.PluginServerWorkerEntryHookABC import \
     PluginServerWorkerEntryHookABC
 from peek_server.plugin.PeekServerPlatformHook import PeekServerPlatformHook
+# from peek_platform.plugin.PluginFrontendInstallerABC import PluginFrontendInstallerABC
+from twisted.internet.defer import inlineCallbacks
 
 logger = logging.getLogger(__name__)
 
@@ -72,7 +70,7 @@ class ServerPluginLoader(PluginLoaderABC):
     def _loadPluginThrows(self, pluginName: str,
                           EntryHookClass: Type[PluginCommonEntryHookABC],
                           pluginRootDir: str,
-                          requiresService: Tuple[str, ...]) -> None:
+                          requiresService: Tuple[str, ...]) -> PluginCommonEntryHookABC:
         # Everyone gets their own instance of the plugin API
         platformApi = PeekServerPlatformHook(pluginName)
 
@@ -87,7 +85,8 @@ class ServerPluginLoader(PluginLoaderABC):
             # Configure the celery app in the worker
             # This is not the worker that will be started, it allows the worker to queue tasks
             from peek_platform.ConfigCeleryApp import configureCeleryApp
-            configureCeleryApp(pluginMain.celeryApp)
+            from peek_platform import PeekPlatformConfig
+            configureCeleryApp(pluginMain.celeryApp, PeekPlatformConfig.config)
 
         # Check the implementation
         elif "worker" in requiresService:
@@ -115,9 +114,6 @@ class ServerPluginLoader(PluginLoaderABC):
             raise Exception("Plugin %s requires 'storage' service."
                             " It must now inherit PluginServerStorageEntryHookMixin"
                             " in its PluginServerEntryHook implementation")
-
-        # Start the Plugin
-        yield pluginMain.start()
 
         # Add all the resources required to serve the backend site
         # And all the plugin custom resources it may create
