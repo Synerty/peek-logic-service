12910ff41411fe973717598b56e0a6884cb31361
Jarrod Chesney <jarrod.chesney@synerty.com>
2017-07-16 20:04:07 +1000

    Fixed winsvc import issues on Linux

--- peek_server/winsvc_peek_server.py
+++ peek_server/winsvc_peek_server.py
@@ -1,23 +1,27 @@
+import platform
 
-import win32serviceutil
-import win32service
-import win32event
-import servicemanager
-# import socket
+try:
+    import win32serviceutil
+    import win32service
+    import win32event
+    import servicemanager
+    # import socket
+except ImportError as e:
+    if platform.system() is "Windows":
+        raise
 
 from twisted.internet import reactor
 
 from peek_server import run_peek_server
 
 
-class PeekSvc (win32serviceutil.ServiceFramework):
+class PeekSvc(win32serviceutil.ServiceFramework):
     _svc_name_ = "peek_server"
     _svc_display_name_ = "Peek Server"
 
-
-    def __init__(self ,args):
-        win32serviceutil.ServiceFramework.__init__(self ,args)
-        self.hWaitStop = win32event.CreateEvent(None ,0 ,0 ,None)
+    def __init__(self, args):
+        win32serviceutil.ServiceFramework.__init__(self, args)
+        self.hWaitStop = win32event.CreateEvent(None, 0, 0, None)
         # socket.setdefaulttimeout(120)
 
     def SvcStop(self):
@@ -28,7 +32,7 @@ class PeekSvc (win32serviceutil.ServiceFramework):
     def SvcDoRun(self):
         servicemanager.LogMsg(servicemanager.EVENTLOG_INFORMATION_TYPE,
                               servicemanager.PYS_SERVICE_STARTED,
-                              (self._svc_name_ ,''))
+                              (self._svc_name_, ''))
         run_peek_server.main()
 
 
