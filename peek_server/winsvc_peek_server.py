7bf0184a98b0e90057acbbd7f6baff9a7d1ebeb0
Jarrod Chesney <jarrod.chesney@synerty.com>
2017-07-16 19:30:35 +1000

    Added windows service entry point

---
+++ peek_server/winsvc_peek_server.py
@@ -0,0 +1,36 @@
+
+import win32serviceutil
+import win32service
+import win32event
+import servicemanager
+# import socket
+
+from twisted.internet import reactor
+
+from peek_server import run_peek_server
+
+
+class PeekSvc (win32serviceutil.ServiceFramework):
+    _svc_name_ = "peek_server"
+    _svc_display_name_ = "Peek Server"
+
+
+    def __init__(self ,args):
+        win32serviceutil.ServiceFramework.__init__(self ,args)
+        self.hWaitStop = win32event.CreateEvent(None ,0 ,0 ,None)
+        # socket.setdefaulttimeout(120)
+
+    def SvcStop(self):
+        self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
+        win32event.SetEvent(self.hWaitStop)
+        reactor.callLater(0, reactor.stop)
+
+    def SvcDoRun(self):
+        servicemanager.LogMsg(servicemanager.EVENTLOG_INFORMATION_TYPE,
+                              servicemanager.PYS_SERVICE_STARTED,
+                              (self._svc_name_ ,''))
+        run_peek_server.main()
+
+
+if __name__ == '__main__':
+    win32serviceutil.HandleCommandLine(PeekSvc)
