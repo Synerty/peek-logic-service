01584b0eb78323fbeb2f543edcff0084a0ff931b
Jarrod Chesney <jarrod.chesney@synerty.com>
2017-11-08 21:58:51 +1000

    Updated to version 0.7.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 pip_package_name = "peek-server"
 py_package_name = "peek_server"
 
-package_version = '0.7.7'
+package_version = '0.7.8'
 
 egg_info = "%s.egg-info" % pip_package_name
 if os.path.isdir(egg_info):
