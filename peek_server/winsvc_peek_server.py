3c64330b9b2413bb23364c225c188c23f7075164
Jarrod Chesney <jarrod.chesney@synerty.com>
2018-04-22 13:54:33 +1000

    Updated winsvc SvcDoRun methods
    ORMOB-600

--- peek_server/winsvc_peek_server.py
+++ peek_server/winsvc_peek_server.py
@@ -1,51 +1,45 @@
-import platform
+import logging
 
-import peek_server
-from peek_platform.util.LogUtil import setupPeekLogger
-
-try:
-    import win32serviceutil
-    import win32service
-    import win32event
-except ImportError as e:
-    if platform.system() is "Windows":
-        raise
-
+import win32service
+import win32serviceutil
 from twisted.internet import reactor
 
-from peek_server import run_peek_server
-
+logger = logging.getLogger(__name__)
 
 class PeekSvc(win32serviceutil.ServiceFramework):
     _svc_name_ = "peek_server"
-    _svc_display_name_ = "Peek Server " + peek_server.__version__
+    _svc_display_name_ = "Peek Server " #+ peek_server.__version__
 
     def __init__(self, args):
         win32serviceutil.ServiceFramework.__init__(self, args)
-        self.hWaitStop = win32event.CreateEvent(None, 0, 0, None)
 
         reactor.addSystemEventTrigger('after', 'shutdown', self._notifyOfStop)
 
-    def _notifyOfStop(self, _):
+    def _notifyOfStop(self):
         self.ReportServiceStatus(win32service.SERVICE_STOPPED)
 
-    def _notifyOfStart(self, _):
+    def _notifyOfStart(self):
         self.ReportServiceStatus(win32service.SERVICE_RUNNING)
 
     def SvcStop(self):
         self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
-        win32event.SetEvent(self.hWaitStop)
         reactor.callFromThread(reactor.stop)
 
     def SvcDoRun(self):
         self.ReportServiceStatus(win32service.SERVICE_START_PENDING)
-        d = run_peek_server.main()
-        d.addBoth(self._notifyOfStart)
-        reactor.run()
+        try:
+
+            reactor.callLater(1, self._notifyOfStart)
+
+            from peek_server import run_peek_server
+            run_peek_server.main()
+
+        except Exception as e:
+            logger.exception(e)
+            raise
 
 
 def main():
-    setupPeekLogger(PeekSvc._svc_name_)
     win32serviceutil.HandleCommandLine(PeekSvc)
 
 
