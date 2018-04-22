671267237e78dc6c2443c17612c035b574544685
Jarrod Chesney <jarrod.chesney@synerty.com>
2018-04-22 16:10:59 +1000

    Added version name back into service
    ORMOB-600

--- peek_server/winsvc_peek_server.py
+++ peek_server/winsvc_peek_server.py
@@ -4,11 +4,15 @@ import win32service
 import win32serviceutil
 from twisted.internet import reactor
 
+import peek_server
+from peek_platform.sw_install.PeekSwInstallManagerABC import IS_WIN_SVC
+
 logger = logging.getLogger(__name__)
 
 class PeekSvc(win32serviceutil.ServiceFramework):
     _svc_name_ = "peek-server"
-    _svc_display_name_ = "Peek Server " #+ peek_server.__version__
+    _svc_display_name_ = "Peek Server " + peek_server.__version__
+    _exe_args_ = IS_WIN_SVC
 
     def __init__(self, args):
         win32serviceutil.ServiceFramework.__init__(self, args)
