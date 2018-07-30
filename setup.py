e6f254154771d5373c3ae2c8b6a81d6a658d7332
Jarrod Chesney <jarrod.chesney@synerty.com>
2018-07-30 16:11:55 +1000

    Updated to version 1.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 pip_package_name = "peek-server"
 py_package_name = "peek_server"
 
-package_version = '1.0.0'
+package_version = '1.1.0'
 
 egg_info = "%s.egg-info" % pip_package_name
 if os.path.isdir(egg_info):
