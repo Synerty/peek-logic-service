e11f49ffcd535b68865355ad1bcfbb52512d56b3
Jarrod Chesney <jarrod.chesney@synerty.com>
2019-06-19 14:53:27 +1000

    Updated package publish scripts.
    PEEK-321

--- peek_server/__init__.py
+++ peek_server/__init__.py
@@ -1,5 +1,5 @@
 __author__ = 'synerty'
-__version__ = '1.3.2'
+__version__ = '0.0.0'
 
 
 def importPackages():
---
+++ publish.settings.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+PY_PACKAGE="peek_server"
+PYPI_PUBLISH="1"
+
+VER_FILES=""
