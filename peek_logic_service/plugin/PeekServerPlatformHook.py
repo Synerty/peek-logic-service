ef24e0fa4dba8fcfade177d5bef60acbcdd2b9f8
Jacob Sorrell <jacob.sorrell@synerty.com>
2020-11-23 10:14:05 +1000

    PEEK-745 Updated Server and Client entry hooks
    - Renamed mobile and desktop functions to field and office
    - Renamed ServerEntryHook to LogicEntryHook
    - Added peekOfficeEntryHook and peekFieldEntryHook

--- peek_logic_service/plugin/PeekServerPlatformHook.py
+++ peek_logic_service/plugin/PeekServerPlatformHook.py
@@ -30,11 +30,11 @@ class PeekServerPlatformHook(PeekServerPlatformHookABC):
         if not otherPlugin:
             return None
 
-        from peek_plugin_base.server.PluginServerEntryHookABC import \
-            PluginServerEntryHookABC
+        from peek_plugin_base.server.PluginLogicEntryHookABC import \
+            PluginLogicEntryHookABC
 
-        assert isinstance(otherPlugin, PluginServerEntryHookABC), (
-            "Not an instance of PluginServerEntryHookABC")
+        assert isinstance(otherPlugin, PluginLogicEntryHookABC), (
+            "Not an instance of PluginLogicEntryHookABC")
 
         return otherPlugin
 
